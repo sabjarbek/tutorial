671ddc750c28f1e853aaedc920b34e4bc43b5ef1
sanjarbek azizov <azizov@gmail.com>
2022-01-31 18:43:03 +0500

    i've done second task

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 function calcAge(year){
-    return 2020-year
+    return 2022-year
 }
-
-console.log(calcAge(1990))
+function infoMan(name, year){
+    const age = calcAge(year)
+    if(age > 0){
+        console.log('my name is ' + name + ', i am ' + age + ' year old')
+    } 
+    else{
+        console.log('error')
+    }    
+}
+infoMan('Sanjar', 1990)
