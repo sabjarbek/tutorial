83b627c921e868ffbf8070638734c4dd0196efc9
sanjarbek azizov <azizov@gmail.com>
2022-05-22 20:41:40 +0500

    add information about information

--- main.js
+++ main.js
@@ -95,7 +95,7 @@ console.log(myname.indexOf('r'))
 console.log(myname.startsWith('san'))
 console.log(myname.endsWith('ar'))
 console.log(myname.repeat(3))
-console.log(myname.trim())*/
+console.log(myname.trim())
 function personData(s, name, age){
 if (age < 0){
  age="еще не родился"
@@ -106,3 +106,70 @@ const personName ="Sankar"
 const personAge = -10
 const output = personData`Имя:${personName}, Возрост: ${personAge} !`
 console.log(output)
+
+// Функции
+
+// function declaration
+function myself (name){
+ console.log('my name is:', name);
+}
+myself('sanjar');
+
+// function Experssion
+const myself2 = function myself2(name) {
+console.log( 'my name is:', name)
+}
+myself2('lilian')
+
+//## разница между function declaration и function Experssion в том что деклорешен можно вызывать вызывать из любой части кода, а функция экспершен можно вызывать только после определения создания функции
+console.log( typeof myself2)
+console.dir(myself) // функция по сути своей это объект
+
+
+//Анонимные функции
+let counter = 0
+const interval = setInterval(
+    function(){
+        if( counter===10){
+            clearInterval(interval)            
+        }
+        else{
+            console.log(++counter)
+        }
+    }
+, 1000)
+*/
+
+// стрелоная функция
+const arrow = (name) =>{
+    console.log('Привет -', name)
+}
+arrow('Sardor')
+const arrow2 = name2 => console.log('hi - I am', name2)
+arrow2('Sanjar')
+
+const pow2=num=> num**2
+console.log(pow2(16))
+const sum =(a=4,b=2)=>a+b
+console.log (sum())
+console.log(sum(6,8))
+
+function sumAll(...all){
+    let result=0;
+    for (let num of all){
+        result+=num
+    }
+    return result
+
+}
+const res = sumAll(1,20,3,48,5,78,7)
+console.log(res)
+
+function createMember(name){
+    return function(lastName){
+        console.log(name + lastName)
+    }
+}
+const logWithLastName = createMember('Azizov')
+console.log(logWithLastName('Sanjar'))
+console.log(logWithLastName('Sardor'))
